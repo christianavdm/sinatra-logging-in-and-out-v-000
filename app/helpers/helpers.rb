1f77a694b6a5587fdc6bfebb173e3ce7bd6a9887
Christiana Vandermale <christianavdm@gmail.com>
2018-09-07 00:42:03 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Helpers
   def self.current_user(params)
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find_by(id: session[:user_id])
   end
 
   def self.is_logged_in?(params)
